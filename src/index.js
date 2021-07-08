b97e7b878336c555eef894bb1161b74f0744dae1
Rahul <talktorahul88@gmail.com>
2021-07-08 20:28:45 +0530

    Added first loaders to handle css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import {run} from "./app/app";
+import "./main.css"; //Whenever webpack will come across this import stmt with .css file, it will check webpack config to see if we have a loader for such a file and if yes, it will use that to bundle this file 
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
