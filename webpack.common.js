6d5ab82a49f8739ea3b314615ce2c6f3036f3c16
Rahul <talktorahul88@gmail.com>
2021-07-08 21:36:39 +0530

    Add prod and dev configs, dev-server
    - Broke our webpack.config file into 3 files
    
    - webpack.common.js, webpack.dev.js, and webpack.prod.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html" //We are telling this plugin to use this file for template and add the script tag with link to our main.js file 
