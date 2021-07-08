72c493baf57983538f96fde981d6994f87f5bd89
Rahul <talktorahul88@gmail.com>
2021-07-08 20:55:25 +0530

    Added sass loaders and overrode few boostrap colors

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
   module: {
     rules: [
         {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            test: /\.scss$/,
+            use: [
+                'style-loader', //3. Injects styles into DOM
+                'css-loader',  //2. Turns css into commonjs
+                'sass-loader' //1. Turns sass into css
+            ]
         }
     ]
   }
